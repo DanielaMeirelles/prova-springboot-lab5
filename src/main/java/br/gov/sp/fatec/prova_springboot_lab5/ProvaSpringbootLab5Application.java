89b491c2602146904fe7c0a81c6b2be9148b89e1
DanielaMeirelles <daniela.meirelles90@outlook.com>
2025-04-07 10:04:56 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/prova_springboot_lab5/ProvaSpringbootLab5Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.prova_springboot_lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaSpringbootLab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaSpringbootLab5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/prova_springboot_lab5/ProvaSpringbootLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.prova_springboot_lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaSpringbootLab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
